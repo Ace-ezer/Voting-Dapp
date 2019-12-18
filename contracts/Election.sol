9220753b262d2c49a6f4c1f76db6f3023e34e5ec
Ace-ezer <yadavrishu771998@gmail.com>
2019-12-18 13:45:02 +0530

    First commit

---
+++ contracts/Election.sol
@@ -0,0 +1,14 @@
+pragma solidity >=0.5.0 <0.7.0;
+
+contract Election {
+    // Store candidate
+    
+
+    //Read candidate
+    string public candidate;
+    
+    // Constructor
+    constructor () public {
+        candidate = "Candidate 1";
+    }
+}
